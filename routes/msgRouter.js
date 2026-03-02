6b78cfb1f506cde43895ca57d7811f05e4c2052a
= <lukejmullins@gmail.com>
2026-03-02 11:35:07 -0500

    Render messages array and render template file for /form

--- routes/msgRouter.js
+++ routes/msgRouter.js
@@ -1,8 +1,23 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 const { Router } = require("express");
 
 const msgRouter = Router();
 
-msgRouter.get("/", (req, res) => res.send("Hello!"));
-msgRouter.get("/new", (req, res) => res.send("Hello!"));
+msgRouter.get("/", (req, res) =>
+  res.render("index", { title: "Mini Messageboard", messages: messages }),
+);
+msgRouter.get("/new", (req, res) => res.render("form"));
 
 module.exports = msgRouter;
