660bdf2dc74113b65532cd075ac3567b15eecbca
= <lukejmullins@gmail.com>
2026-03-03 10:11:07 -0500

    Add css to home page

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const msgRouter = require("./routes/msgRouter");
+const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 app.use("/", msgRouter);
 
