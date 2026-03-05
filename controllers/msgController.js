1479ab9982bb2e6b41bd6fe336a605e06e5778ac
= <lukejmullins@gmail.com>
2026-03-05 12:30:41 -0500

    Add validatiom

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -1,23 +1,47 @@
 const db = require("../db/queries");
 const CustomNotFoundError = require("../errors/CustomNotFoundError");
+const { body, validationResult, matchedData } = require("express-validator");
 
-async function createMsg(req, res) {
-  const { text, user } = req.body;
-  const added = Intl.DateTimeFormat("en-US", {
-    timeStyle: "short",
-    dateStyle: "short",
-  }).format(new Date());
+const textErr = "must be 200 characters or less.";
+const userErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 20 characters.";
 
-  await db.insertPost(text, user, added);
-  res.redirect("/");
-}
+const validateUser = [
+  body("text").isLength({ max: 200 }).withMessage(`Text ${textErr}`),
+  body("user")
+    .trim()
+    .isAlpha()
+    .withMessage(`User ${userErr}`)
+    .isLength({ min: 1, max: 20 })
+    .withMessage(`User ${lengthErr}`),
+];
+
+const createMsg = [
+  validateUser,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("form", {
+        errors: errors.array(),
+      });
+    }
+
+    const { text, user } = req.body;
+    const added = Intl.DateTimeFormat("en-US", {
+      timeStyle: "short",
+      dateStyle: "short",
+    }).format(new Date());
+
+    await db.insertPost(text, user, added);
+    res.redirect("/");
+  },
+];
 
 const renderForm = (req, res) => res.render("form");
 
 async function renderIndex(req, res) {
   const messages = await db.getAllPosts();
-
-  console.log(messages);
 
   res.render("index", {
     title: "Mini Messageboard",
