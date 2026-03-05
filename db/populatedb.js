03a02ec3b91074d326ed8aa04d0c6e2771c74fb9
= <lukejmullins@gmail.com>
2026-03-05 09:29:50 -0500

    Connect to db

---
+++ db/populatedb.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  text VARCHAR ( 255 ),
+  username VARCHAR ( 255 ),
+  added VARCHAR ( 255 )
+
+);
+
+INSERT INTO messages (text,username,added) 
+VALUES 
+  ('Hi there!', 'Amando', '${new Date()}'),
+  ('Hello World!', 'Charles', '${new Date()}');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: `${process.env.URL}`,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
